7827862ac51b24c4f1e64c741a7fd734987951b7
mrantika <94685047+mrantikadev@users.noreply.github.com>
2025-02-17 14:42:38 +0300

    Added DTOs and DTO Mappers for Stock entity

---
+++ Api/Mappers/StockMappers.cs
@@ -0,0 +1,22 @@
+﻿using Api.Dtos.Stock;
+using Api.Models;
+
+namespace Api.Mappers
+{
+    public static class StockMappers
+    {
+        public static StockDto ToStockDto(this Stock stockModel)
+        {
+            return new StockDto
+            {
+                StockId = stockModel.StockId,
+                Symbol = stockModel.Symbol,
+                CompanyName = stockModel.CompanyName,
+                Purchase = stockModel.Purchase,
+                LastDiv = stockModel.LastDiv,
+                Industry = stockModel.Industry,
+                MarketCap = stockModel.MarketCap
+            };
+        }
+    }
+}
