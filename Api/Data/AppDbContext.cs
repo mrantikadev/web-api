6b64a35383350ff792fe5a43b26d02adfed15d7f
mrantika <94685047+mrantikadev@users.noreply.github.com>
2025-02-17 13:49:54 +0300

    mig

---
+++ Api/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options)
+        : base(options)
+        {
+            
+        }
+
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
