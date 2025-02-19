861b61556c54b68f5b9fa0a12d06f3e3a7c690f0
mrantika <94685047+mrantikadev@users.noreply.github.com>
2025-02-19 12:42:30 +0300

    Added Cascade Delete to delete all the Comments attached to a Stock when the Stock is deleted

--- Api/Data/AppDbContext.cs
+++ Api/Data/AppDbContext.cs
@@ -13,5 +13,16 @@ namespace Api.Data
 
         public DbSet<Stock> Stocks { get; set; }
         public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Comment>()
+                .HasOne(c => c.Stock)
+                .WithMany(s => s.Comments)
+                .HasForeignKey(c => c.StockId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ Api/Migrations/20250219093914_AddCascadeDelete.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCascadeDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Stocks_StockId",
+                table: "Comments");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Stocks_StockId",
+                table: "Comments",
+                column: "StockId",
+                principalTable: "Stocks",
+                principalColumn: "StockId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Stocks_StockId",
+                table: "Comments");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Stocks_StockId",
+                table: "Comments",
+                column: "StockId",
+                principalTable: "Stocks",
+                principalColumn: "StockId");
+        }
+    }
+}
